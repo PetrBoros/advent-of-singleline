8574b24e9bf2d80de4e7b63cfe4ee8fe51fd85c4
petrboros <petr@bori.cz>
2016-12-07 22:32:00 +0100

    ported to Python 3

--- 1/singleline.py
+++ 1/singleline.py
@@ -1,2 +1,4 @@
+from functools import reduce
+
 # Part 1
-print (lambda x: abs(x[0])+abs(x[1]))(reduce(lambda s, i: (lambda s, i: [s[0],s[1]+int(i[1:]),s[2]] if s[2] == 0 else ([s[0],s[1]-int(i[1:]),s[2]] if s[2] == 2 else ([s[0]+int(i[1:]),s[1],s[2]] if s[2] == 1 else [s[0]-int(i[1:]),s[1],s[2]])))((lambda s, i: [s[0],s[1],(s[2]-1)%4] if i[0] == 'L' else [s[0],s[1],(s[2]+1)%4])(s, i), i), open('input.txt').read().split(', '), [0,0,0]))
+print((lambda x: abs(x[0])+abs(x[1]))(reduce(lambda s, i: (lambda s, i: [s[0],s[1]+int(i[1:]),s[2]] if s[2] == 0 else ([s[0],s[1]-int(i[1:]),s[2]] if s[2] == 2 else ([s[0]+int(i[1:]),s[1],s[2]] if s[2] == 1 else [s[0]-int(i[1:]),s[1],s[2]])))((lambda s, i: [s[0],s[1],(s[2]-1)%4] if i[0] == 'L' else [s[0],s[1],(s[2]+1)%4])(s, i), i), open('input.txt').read().split(', '), [0,0,0])))
--- 7/singleline.py
+++ 7/singleline.py
@@ -1,9 +1,10 @@
 import re
 from operator import or_, add
 from itertools import chain
+from functools import reduce
 
 # Part 1
-print sum([1 if x == (True, False) else 0 for x in map(lambda x: (reduce(or_, x[0::2]),reduce(or_, x[1::2])), map(lambda r: [reduce(or_, [x[a] == x[a+3] and x[a+1] == x[a+2] and x[a] != x[a+1] for a in range(len(x)-3)]) for x in r], [filter(None, re.findall('([a-z]*)', row)) for row in open('input.txt').read().splitlines()]))])
+print(sum([1 if x == (True, False) else 0 for x in map(lambda x: (reduce(or_, x[0::2]),reduce(or_, x[1::2])), map(lambda r: [reduce(or_, [x[a] == x[a+3] and x[a+1] == x[a+2] and x[a] != x[a+1] for a in range(len(x)-3)]) for x in r], [filter(None, re.findall('([a-z]*)', row)) for row in open('input.txt').read().splitlines()]))]))
 
 # Part 2
-print sum([1 if len(x-y) < len(x) else 0 for (x,y) in map(lambda x: (set(reduce(add, [a for a in x[0::2]])), set(map(lambda t: t[1]+t[0]+t[1], filter(lambda o: o[0] != o[1], reduce(add, [a for a in x[1::2]]))))), [map(lambda x: list(chain([a for (a,b) in re.findall(r'(?=((.).\2))', x)])), filter(None, re.findall('([a-z]*)', row))) for row in open('input.txt').read().splitlines()])])
+print(sum([1 if len(x-y) < len(x) else 0 for (x,y) in map(lambda x: (set(reduce(add, [a for a in x[0::2]])), set(map(lambda t: t[1]+t[0]+t[1], filter(lambda o: o[0] != o[1], reduce(add, [a for a in x[1::2]]))))), [list(map(lambda x: list(chain([a for (a,b) in re.findall(r'(?=((.).\2))', x)])), filter(None, re.findall('([a-z]*)', row)))) for row in open('input.txt').read().splitlines()])]))
