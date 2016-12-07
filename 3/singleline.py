615816850d4b111e3a72daa8139058ce22c1372c
petrboros <petr@bori.cz>
2016-12-07 18:10:17 +0100

    problem 3

---
+++ 3/singleline.py
@@ -0,0 +1,7 @@
+from itertools import chain
+
+# Part 1
+print sum(map(lambda (a,b,c): 1 if a+b > c else 0, [tuple(sorted(map(int, x.split()))) for x in open('input.txt').read().splitlines()]))
+
+# Part 2
+print sum(map(lambda (a,b,c): 1 if a+b > c else 0, (lambda x: [tuple(sorted(x[i:i+3])) for i in range(0,len(x)-2,3)])(list(chain(*map(list, zip(*[map(int, x.split()) for x in open('input.txt').read().splitlines()])))))))
